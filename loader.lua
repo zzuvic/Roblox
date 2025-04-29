bec9567c6c638b2a1623067a7b42b196d29ef6fd
zzuvic <agustinzuvic@gmail.com>
2025-04-30 00:23:23 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -15,6 +15,7 @@ shared.Settings = {
     ['Theme'] = true,-- Halloween = Halloween Mode, true == Dark Mode, false == White Mode
     ['Key'] = 'RightShift',--Key to toggle ui.
     Options = {
+IUDfgYzsoTPwMvCqVwkgrXobhFtXoDLp
         ['ServerHop'] = false, --// Set this on before setting "BossFarming" to true
         ['BossFarming'] = false, --// Make sure serverhop is on or it wont work.
     }
